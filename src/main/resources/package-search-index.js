c17beaa67a623ea151bf2fa882b303570e8be892
mario155321 <mariow@datazug.ch>
2023-01-13 12:58:04 +0100

    add javadoc to git

---
+++ src/main/resources/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ch.teko.wema"}];updateSearchResults();
---
+++ src/main/resources/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"ch.teko.wema","l":"Trie"}];updateSearchResults();
