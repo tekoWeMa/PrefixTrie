458da17015a27f44b085274a0ad33291543bda18
tekoWeMa <98219925+tekoWeMa@users.noreply.github.com>
2023-03-17 15:52:28 +0100

    remove javadoc

--- src/main/resources/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ch.teko.wema"}];updateSearchResults();
--- src/main/resources/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"ch.teko.wema","l":"Trie"}];updateSearchResults();
